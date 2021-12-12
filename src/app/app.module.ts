1182981ca9fcb49f9c7101d349c6f90aee6cf620
rogerioTadeuDosReis <rogerio.rtr46@gmail.com>
2021-12-11 21:44:53 -0300

    Criação do arquivo de rotas manualmente
    Criado o arquivo de rotas para navegaçãos dos itens da aplicação,
    Imports necessários para navegação no app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
+import { APP_BASE_HREF } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { rootRouterConfig } from './app.routes';
+import { ContatoComponent } from './institucional/contato/contato.component';
+import { SobreComponent } from './institucional/sobre/sobre.component';
+import { FooterComponent } from './navegacao/footer/footer.component';
+import { HomeComponent } from './navegacao/home/home.component';
+import { MenuComponent } from './navegacao/menu/menu.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    HomeComponent,
+    FooterComponent,
+    SobreComponent,
+    ContatoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    [RouterModule.forRoot(rootRouterConfig, { useHash: false })]
   ],
-  providers: [],
+  providers: [
+    { provide: APP_BASE_HREF, useValue: '/' }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
